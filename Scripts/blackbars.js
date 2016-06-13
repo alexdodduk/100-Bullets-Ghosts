406bd62880ad4082678555594339e32483f7f405
Alex Dodd <adodd@live.co.uk>
2016-06-13 19:06:31 +0100

    Updated the donate link and added a video to the watch page

--- Scripts/blackbars.js
+++ Scripts/blackbars.js
@@ -1,24 +1,29 @@
 $(document).ready(function () {
-    var bodyHeight = $('body').innerHeight();
-    var windowHeight = window.innerHeight;
+    function addBlackBars() {
+        var bodyHeight = $('body').innerHeight();
+        var windowHeight = window.innerHeight;
 
-    if (bodyHeight > windowHeight) {
+        if (bodyHeight > windowHeight) {
 
-        var padding = 500;
-        while (padding >= 100 && bodyHeight > windowHeight) {
-            padding -= 1;
-            $('body').css('padding-top', padding + 'px').css('padding-bottom', padding + 'px');
+            var padding = 500;
+            while (padding >= 100 && bodyHeight > windowHeight) {
+                padding -= 1;
+                $('body').css('padding-top', padding + 'px').css('padding-bottom', padding + 'px');
 
-            bodyHeight = $('body').innerHeight();
-        }
-    } else {
+                bodyHeight = $('body').innerHeight();
+            }
+        } else {
 
-        var padding = 0;
-        while (bodyHeight < windowHeight - 20) {
-            padding += 1;
-            $('body').css('padding-top', padding + 'px').css('padding-bottom', padding + 'px');
+            var padding = 0;
+            while (bodyHeight < windowHeight - 20) {
+                padding += 1;
+                $('body').css('padding-top', padding + 'px').css('padding-bottom', padding + 'px');
 
-            bodyHeight = $('body').innerHeight();
+                bodyHeight = $('body').innerHeight();
+            }
         }
     }
+    
+    $(window).resize(addBlackBars);
+    $(window).trigger("resize");
 });
